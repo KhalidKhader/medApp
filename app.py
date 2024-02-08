3b65b4dcdb771ce8c517376f43b74bb5f001f72c
Khalid Khader <khalidkhader@Khalids-MacBook-Pro.local>
2024-02-08 21:43:41 +0200

    ready for deploy

--- app.py
+++ app.py
@@ -199,7 +199,8 @@ def predict_Alzeh():
 
 if __name__ == '__main__':
     # app.run(host="0.0.0.0",port=5000)
-    app.run(debug=True,port=4000)
+    # app.run(debug=True,port=4000)
+    app.run()
 
 
 
